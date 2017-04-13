24040a07c24b6444548547b7d43d42ff57882803
HUBERTWANG-PC\jingjingwang <wjj0508403034@126.com>
2017-04-13 23:31:41 +0800

    adminlte

--- gulpfile.js
+++ gulpfile.js
@@ -11,53 +11,62 @@ var inject = require('gulp-inject');
 const DestFolder = "dist";
 
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
   return del([DestFolder]);
 });
 
 
 gulp.task('default', ['build']);
 
-gulp.task('build-css', ['clean'], function() {
+gulp.task('build-css', ['clean'], function () {
   return gulp.src('src/**/*.css')
     .pipe(showFile())
     .pipe(concat("app.css"))
     .pipe(gulp.dest(DestFolder));
 });
 
-gulp.task('build-js', ['clean'], function() {
+gulp.task('build-js', ['clean'], function () {
   return gulp.src('src/**/*.js')
     .pipe(showFile())
     .pipe(concat("app.js"))
     .pipe(gulp.dest(DestFolder));
 });
 
-gulp.task('copy-resource', ['clean'], function() {
-  return gulp.src('res/**')
+gulp.task('copy-resource', ['clean'], function () {
+  return gulp.src(['res/**',
+    'libs/**/*.jpg',
+    'libs/**/*.png'
+  ])
     .pipe(showFile())
     .pipe(gulp.dest(`${DestFolder}/res`));
 });
 
-gulp.task('copy-thirdparty', ['clean'], function() {
+gulp.task('copy-font', ['clean'], function () {
+  return gulp.src(['fonts/**'])
+    .pipe(showFile())
+    .pipe(gulp.dest(`${DestFolder}/fonts`));
+});
+
+gulp.task('copy-thirdparty', ['clean'], function () {
   return gulp.src('libs/**')
     .pipe(showFile())
     .pipe(gulp.dest(`${DestFolder}/libs`));
 });
 
-gulp.task('build', ['copy-thirdparty', 'copy-resource', 'build-css', 'build-js'], function() {
+gulp.task('build', ['copy-thirdparty', 'copy-resource','copy-font', 'build-css', 'build-js'], function () {
   var injectCss = gulp.src([
     `${DestFolder}/**/*.css`
   ], {
-    read: false
-  });
+      read: false
+    });
 
   var injectJs = gulp.src([
     `${DestFolder}/libs/jquery.min.js`, // 必须把jquery放在第一个文件，后面很多模块依赖jquery
     `${DestFolder}/libs/angular.js`,
     `${DestFolder}/**/*.js`
   ], {
-    read: false
-  });
+      read: false
+    });
 
   return gulp.src('src/**/*.html')
     .pipe(showFile())
