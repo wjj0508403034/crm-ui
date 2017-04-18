4b61baaa8cf7b0e6cdb694e44171820afc963815
wangxiaochuan <1576984609@qq.com>
2017-04-18 22:42:57 +0800

    导航栏和侧栏修改

--- src/common/sidebar/sidebar.js
+++ src/common/sidebar/sidebar.js
@@ -3,6 +3,7 @@
 huoyunWidget.directive('widgetSidebar', ['$http',function ($http) {
   return {
     restrict: 'A',
+    replace:true,
     scope: {},
     templateUrl: 'sidebar/sidebar.html',
     link: function (scope, ele, attrs) {
