c23be1251f737ceb5aa079c9ddb0cb48c4809e71
wangxiaochuan <1576984609@qq.com>
2017-05-23 23:15:18 +0800

    排序调整

--- src/main/view/framework/sortListView.js
+++ src/main/view/framework/sortListView.js
@@ -4,22 +4,47 @@ huoyunWidget.controller('SortListViewController', ['$scope', "BoService",
   function($scope, BoService) {
     var boMetadata = $scope.ngDialogData.params.boMetadata;
     $scope.pageData = $scope.ngDialogData.params.pageData.slice(0);
-    $scope.onMoveRow = function(direction, index) {
+    /**
+     * 单击tr，标记当前行号，设置选中样式
+     */
+    $scope.clickRow=function(index){
+      $scope.sIndex=index;
+      //上移按钮状态
+      if(index===0){
+        $scope.isMoveUpDisabled=true;
+      }else{
+        $scope.isMoveUpDisabled=false;
+      }
+      //下移按钮状态
+      if(index===$scope.pageData.length-1){
+        $scope.isMoveDownDisabled=true;
+      }else{
+        $scope.isMoveDownDisabled=false;
+      }
+    };
+    /**
+     * 上移，下移操作
+     */
+    $scope.onMoveButtonClicked = function(direction) {
       if (direction === 'up') {
-        if (index === 0) {
+        if ($scope.sIndex === 0) {
           return;
         }
-        var temp = $scope.pageData[index - 1];
-        $scope.pageData[index - 1] = $scope.pageData[index];
-        $scope.pageData[index] = temp;
+        var temp = $scope.pageData[$scope.sIndex - 1];
+        $scope.pageData[$scope.sIndex - 1] = $scope.pageData[$scope.sIndex];
+        $scope.pageData[$scope.sIndex] = temp;
+        //设置当前移动项被选中，方便继续移动
+        $scope.clickRow($scope.sIndex-1);
       } else {
         var length = $scope.pageData.length;
-        if (index === length - 1) {
+        if ($scope.sIndex === length - 1) {
           return;
         }
-        var temp = $scope.pageData[index + 1];
-        $scope.pageData[index + 1] = $scope.pageData[index];
-        $scope.pageData[index] = temp;
+        var temp = $scope.pageData[$scope.sIndex + 1];
+        $scope.pageData[$scope.sIndex + 1] = $scope.pageData[$scope.sIndex];
+        $scope.pageData[$scope.sIndex] = temp;
+        //设置当前移动项被选中，方便继续移动
+        $scope.clickRow($scope.sIndex+1);
       }
     };
 
